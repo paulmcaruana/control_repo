4a1da513f4d3287dcb5bc1d4a214db6fe34a46e6
Paul Caruana <57869924+paulmcaruana@users.noreply.github.com>
2020-05-11 17:03:16 +0100

    Update agent-nodes.pp

--- site/profile/manifests/agent-nodes.pp
+++ site/profile/manifests/agent-nodes.pp
@@ -5,4 +5,9 @@ class profile::agent_nodes {
   host {'web.puppet.vm':
     ensure => present,
     ip => '10.0.2.15'
-}  
+  }
+  host {'db.puppet.vm':
+    ensure => present,
+    ip => '10.0.2.37'
+  }
+}
