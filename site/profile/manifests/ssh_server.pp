c74d544ad8a4b706b362a5f8c003fef018cef112
Paul Caruana <57869924+paulmcaruana@users.noreply.github.com>
2020-05-11 17:46:33 +0100

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
     ensure => present,
     user   => 'root',
     type   => 'ssh-rsa',
-    key    => '',
+    key    => ' AAAAB3NzaC1yc2EAAAADAQABAAABAQCwpIE1CAQPmhXc0jjvglQLuz0B4kIzvixKHU8rbgDmXIWwLpeep8YExSUea1oPkJcJgzyYAcRyS7dKUQat2Dxl7RQzaIvLTK0ACc4Lk1L995ogiBq2QqFlxCIMGzNRl6dc5s+kiVSfJ4YIpHUYRVL1e5yexCJdt+xf2nOSdvZpzvMfTCqshGCdeF5CugtgZxeYSKaCOa5SpPRXtBZcwc1l2e1LnF42cyddESijfOD0RZ8DCzafJDEa0Wx1xC2/c1/igBEl37Emo4cY+T1/vF3MVHLt6ATgc3pilkzWidKmOGaOrJNJJYxiC7HizD7G21F0P/Hviu4xDLgGbs9RYnbT',
   }  
 }
