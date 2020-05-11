340214577e5d82b1ddf304ab0e97576f72777dc2
Paul Caruana <57869924+paulmcaruana@users.noreply.github.com>
2020-05-11 18:58:11 +0100

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,9 +1,10 @@
-class minecraft {
-  $url = 'https://s3.amazonaws.com/Minecraft_Download/versions/1.12.2/minecraft_server.1.12.2.jar'
+class minecraft (
+  $url = 'https://s3.amazonaws.com/Minecraft_Download/versions/1.12.2/minecraft_server.1.12.2.jar',
   $install_dir = '/opt/minecraft'
+  ){
   file {$install_dir:
     ensure => directory,
-  }
+    }
   file {"${install_dir}/minecraft_server.jar":
     ensure => file,
     source => $url,
